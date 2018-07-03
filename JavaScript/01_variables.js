a58d3e6e877bececb5c774044df85cd7d6c3ac74
NauelG <g.nauel@gmail.com>
2018-07-03 20:27:50 +0200

    Cuarta clase

---
+++ JavaScript/01_variables.js
@@ -0,0 +1,21 @@
+console.log('Hola Mundo');
+
+// ** Variables **
+// Ámbios: funciones y bloques.
+
+// ES5: var ES6: let, const
+
+let num = 0;
+
+function sumar() {
+    let x = 1; {
+        let n = 0
+        x += n
+    }
+    console.log(x);
+}
+
+sumar()
+
+// *** Tipos de datos ***
+// Number, String, Boolean, Undefined (Y null), Object (y Arrays)
